31abb00d24f4e2182122bc5b98cd1dd5c4bda565
tylerweitzman <tylerweitzman@gmail.com>
2019-12-16 15:18:54 -0800

    re-add glass d

---
+++ glass-daemon/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/hashicorp/errwrap"
+	"github.com/kardianos/service"
+)
+
+var Version = "0.0.0"
+var Build = "gobuild"
+
+type daemon struct {
+	keeper *Keeper
+	server *Server
+}
+
+func (p *daemon) Start(s service.Service) error {
+	var err error
+
+	path, err := SystemTimeglassPathCreateIfNotExist()
+	if err != nil {
+		return errwrap.Wrapf("Failed to find Timeglass system path: {{err}}", err)
+	}
+
+	p.keeper, err = NewKeeper(path)
+	if err != nil {
+		return errwrap.Wrapf("Failed to create time keeper: {{err}}", err)
+	}
+
+	bind := "127.0.0.1:3838"
+	p.server, err = NewServer(bind, p.keeper)
+	if err != nil {
+		return errwrap.Wrapf(fmt.Sprintf("Failed to create server on '%s': {{err}}, is the service already running?", bind), err)
+	}
+
+	go p.server.checkVersion()
+	go p.keeper.Start()
+	go p.run()
+	return nil
+}
+
+func (p *daemon) Stop(s service.Service) error {
+	p.keeper.Stop()
+	return p.server.Stop()
+}
+
+func (p *daemon) run() error {
+	return p.server.Start()
+}
+
+func main() {
+	flag.Parse()
+
+	//setup logging to a file
+	l, err := NewLogger(os.Stderr)
+	if err != nil {
+		log.Fatalf("Failed to create logger: %s", err)
+	}
+
+	log.SetOutput(l)
+	defer l.Close()
+
+	//initialize service
+	conf := &service.Config{
+		Name:        "timeglass",
+		DisplayName: "Timeglass",
+		Description: "Automated time tracking daemon that monitors file changes",
+		Option:      map[string]interface{}{},
+	}
+
+	if runtime.GOOS == "darwin" {
+		conf.Name = "com.timeglass.glass-daemon"
+
+		// @todo technically this is possible but
+		// results in a automated installer that
+		// has some serious usability issues
+		// conf.Option["UserService"] = true
+	} else if runtime.GOOS == "windows" {
+
+		//WATCH OUT: timeglass has a windows installer
+		//that takes care of installing and starting services
+		//in addition to the command line
+
+		conf.Name = "Timeglass" //windows style
+	}
+
+	d := &daemon{}
+	s, err := service.New(d, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//handle service controls
+	if len(flag.Args()) > 0 {
+		err = service.Control(s, flag.Args()[0])
+		if err != nil {
+			ReportServiceControlErrors(err)
+		}
+		return
+	}
+
+	//start daemon
+	log.Printf("Daemon launched, writing logs to '%s'", l.Path())
+	defer func() {
+		log.Printf("Daemon terminated\n\n")
+	}()
+
+	err = s.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ glass-daemon/server.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/hashicorp/errwrap"
+)
+
+var CheckVersionURL = "https://s3-eu-west-1.amazonaws.com/timeglass/version/VERSION?dversion=" + Version
+
+type Server struct {
+	keeper            *Keeper
+	httpb             string
+	listener          net.Listener
+	mostRecentVersion string
+
+	*http.Server
+}
+
+func (s *Server) timersDelete(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.Respond(w, err)
+		return
+	}
+
+	if dirs, ok := r.Form["dir"]; !ok {
+		s.Respond(w, fmt.Errorf("dir parameter is mandatory"))
+		return
+	} else {
+		for _, dir := range dirs {
+			err := s.keeper.Remove(dir)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed to remove timer: {{err}}", err))
+				return
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *Server) timersCreate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.Respond(w, err)
+		return
+	}
+
+	if dirs, ok := r.Form["dir"]; !ok {
+		s.Respond(w, fmt.Errorf("dir parameter is mandatory"))
+		return
+	} else {
+		for _, dir := range dirs {
+			t, err := NewTimer(dir)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed to create new timer: {{err}}", err))
+				return
+			}
+
+			err = s.keeper.Add(t)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed to add new timer to keeper: {{err}}", err))
+				return
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (s *Server) timersPause(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.Respond(w, err)
+		return
+	}
+
+	if dirs, ok := r.Form["dir"]; !ok {
+		s.Respond(w, fmt.Errorf("dir parameter is mandatory"))
+		return
+	} else {
+		for _, dir := range dirs {
+			t, err := s.keeper.Get(dir)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed get timer: {{err}}", err))
+				return
+			}
+
+			t.Pause()
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *Server) timersReset(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.Respond(w, err)
+		return
+	}
+
+	if dirs, ok := r.Form["dir"]; !ok {
+		s.Respond(w, fmt.Errorf("dir parameter is mandatory"))
+		return
+	} else {
+		for _, dir := range dirs {
+			t, err := s.keeper.Get(dir)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed get timer: {{err}}", err))
+				return
+			}
+
+			t.Reset()
+		}
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *Server) timersInfo(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		s.Respond(w, err)
+		return
+	}
+
+	timers := []*Timer{}
+	if dirs, ok := r.Form["dir"]; !ok {
+		s.Respond(w, fmt.Errorf("dir parameter is mandatory"))
+		return
+	} else {
+		for _, dir := range dirs {
+			t, err := s.keeper.Get(dir)
+			if err != nil {
+				s.Respond(w, errwrap.Wrapf("Failed to get timer: {{err}}", err))
+				return
+			}
+
+			timers = append(timers, t)
+		}
+	}
+
+	s.Respond(w, timers)
+}
+
+func (s *Server) api(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"build":          Build,
+		"version":        Version,
+		"newest_version": s.mostRecentVersion,
+		"keeper":         s.keeper,
+	}
+
+	go s.checkVersion()
+	s.Respond(w, data)
+}
+
+func NewServer(httpb string, keeper *Keeper) (*Server, error) {
+	l, err := net.Listen("tcp", httpb)
+	if err != nil {
+		return nil, errwrap.Wrapf(fmt.Sprintf("Failed to create listener on '%s': {{err}}", httpb), err)
+	}
+
+	mux := http.NewServeMux()
+	s := &Server{
+		keeper:   keeper,
+		httpb:    httpb,
+		listener: l,
+
+		Server: &http.Server{Handler: mux},
+	}
+
+	mux.HandleFunc("/api/", s.api)
+	mux.HandleFunc("/api/timers.create", s.timersCreate)
+	mux.HandleFunc("/api/timers.pause", s.timersPause)
+	mux.HandleFunc("/api/timers.delete", s.timersDelete)
+	mux.HandleFunc("/api/timers.reset", s.timersReset)
+	mux.HandleFunc("/api/timers.info", s.timersInfo)
+	return s, nil
+}
+
+func (s *Server) Addr() string {
+	return s.listener.Addr().String()
+}
+
+func (s *Server) Stop() error {
+	return s.listener.Close()
+}
+
+func (s *Server) Start() error {
+	log.Printf("Started server on %s", s.Addr())
+	defer func() {
+		log.Printf("Stopped server on %s", s.Addr())
+	}()
+
+	return s.Server.Serve(s.listener)
+}
+
+func (s *Server) Respond(w http.ResponseWriter, data interface{}) {
+	var err error
+
+	enc := json.NewEncoder(w)
+	if derr, ok := data.(error); !ok {
+		err = enc.Encode(data)
+	} else {
+		err = derr
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(map[string]string{
+			"error": err.Error(),
+		})
+	}
+}
+
+func (s *Server) checkVersion() {
+	resp, err := http.Get(CheckVersionURL)
+	if err == nil {
+		defer resp.Body.Close()
+		buff := bytes.NewBuffer(nil)
+		_, err = io.Copy(buff, resp.Body)
+		if err != nil {
+			log.Printf("Failed to read response body for version check: %s", err)
+		} else {
+			s.mostRecentVersion = strings.TrimSpace(buff.String())
+		}
+	}
+}
